3866287967a01eff1ea96d226fc73775e9575f2c
KChanHyeok <kang9kang9@gmail.com>
2022-09-28 16:23:33 +0900

    정리

--- javatest/src/filetest/FileTest.java
+++ javatest/src/filetest/FileTest.java
@@ -1,9 +1,39 @@
 package filetest;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class FileTest {
     public static void main(String[] args) {
-        File
+        Scanner scan = new Scanner(System.in);
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+            File folder = new File("data");
+            if(!folder.isDirectory()) {
+                folder.mkdir();
+            }
+            File file = new File("data\\test.txt");
+            fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            System.out.print("내용 작성 : ");
+            String str = scan.nextLine();
+            bw.write(str);
+            bw.flush();
+            System.out.println("저장성공");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                bw.close();
+                fw.close();
+            }catch (IOException ie){
+                ie.printStackTrace();
+            }
+        }
     }
 }
